263a575f480382de8fa06bbf90d04c84d0e446aa
SeleminUriy <prepress-s@selemin.ru>
2024-11-07 01:12:12 +0300

    Commit1

--- DjangoMod_19/task1/models.py
+++ DjangoMod_19/task1/models.py
@@ -10,8 +10,8 @@ class Buyer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=200)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(decimal_places=2)
+    size = models.DecimalField(decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer)
