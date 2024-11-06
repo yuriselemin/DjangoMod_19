0dd2b583d342864aa40f82d3f2e1dec6aae72634
SeleminUriy <prepress-s@selemin.ru>
2024-11-07 01:04:47 +0300

    Commit

--- DjangoMod_19/task1/models.py
+++ DjangoMod_19/task1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer)
+
+
