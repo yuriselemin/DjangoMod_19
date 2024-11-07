ece30e4c9eb564c5f4eb7e438770a6538f249a0f
SeleminUriy <prepress-s@selemin.ru>
2024-11-07 23:03:50 +0300

    Commit1

--- DjangoMod_19/task1/models.py
+++ DjangoMod_19/task1/models.py
@@ -21,28 +21,33 @@ class Game(models.Model):
 
 
 """
-from django.db.models import Q
+QuerySet запросы в порядке вызовов:
 
-# Create 3 Buyer records
-buyer1 = Buyer.objects.create(name='Buyer 1', balance=100.00, age=25)
-buyer2 = Buyer.objects.create(name='Buyer 2', balance=200.00, age=17)
-buyer3 = Buyer.objects.create(name='Buyer 3', balance=300.00, age=30)
 
-# Create 3 Game records
-game1 = Game.objects.create(title='Game 1', cost=50.00, size=1.5, description='Game 1 description', age_limited=False)
-game2 = Game.objects.create(title='Game 2', cost=75.00, size=2.0, description='Game 2 description', age_limited=True)
-game3 = Game.objects.create(title='Game 3', cost=100.00, size=3.0, description='Game 3 description', age_limited=False)
-
-# Assign Buyer records to Game records
+1.
+Buyer.objects.create(name='Buyer 1', balance=100.00, age=25)
+2.
+Buyer.objects.create(name='Buyer 2', balance=200.00, age=17)
+3.
+Buyer.objects.create(name='Buyer 3', balance=300.00, age=30)
+4.
+Game.objects.create(title='Game 1', cost=50.00, size=1.5, description='Game 1 description', age_limited=False)
+5.
+Game.objects.create(title='Game 2', cost=75.00, size=2.0, description='Game 2 description', age_limited=True)
+6.
+Game.objects.create(title='Game 3', cost=100.00, size=3.0, description='Game 3 description', age_limited=False)
+7.
 game1.buyer.set([buyer1, buyer2, buyer3])
+8.
 game2.buyer.set([buyer1, buyer2])
+9.
 game3.buyer.set([buyer1, buyer3])
+10.
+Buyer.objects.filter(age__lt=18)
+11.
+Game.objects.filter(age_limited=True)
+12.
+games_with_age_limit.exclude(buyer=under_18_buyer).update(buyer=buyer1)
 
-# Ensure that only one Buyer has all Game.Buyer with age less than 18 and does not have games with age_limited=True
-under_18_buyers = Buyer.objects.filter(age__lt=18)
-if under_18_buyers.count() == 1:
-    under_18_buyer = under_18_buyers.first()
-    games_with_age_limit = Game.objects.filter(age_limited=True)
-    if games_with_age_limit.count() > 0:
-        games_with_age_limit.exclude(buyer=under_18_buyer).update(buyer=buyer1) 
+
 """    
