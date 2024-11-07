ab5af96829ab62d21878a5ec057ed6727befe6d7
SeleminUriy <prepress-s@selemin.ru>
2024-11-07 23:08:40 +0300

    Commit_07_11

--- DjangoMod_19/task1/models.py
+++ DjangoMod_19/task1/models.py
@@ -14,9 +14,38 @@ class Game(models.Model):
     size = models.DecimalField(decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer)
+    buyer = models.ManyToManyField(Buyer, related_name="games")
 
     def __str__(self):
         return self.title
 
+"""
+QuerySet запросы в порядке вызовов:
 
+1.
+Buyer.objects.create(name='Buyer 1', balance=100.00, age=25)
+2.
+Buyer.objects.create(name='Buyer 2', balance=200.00, age=17)
+3.
+Buyer.objects.create(name='Buyer 3', balance=300.00, age=30)
+4.
+Game.objects.create(title='Game 1', cost=50.00, size=1.5, description='Game 1 description', age_limited=False)
+5.
+Game.objects.create(title='Game 2', cost=75.00, size=2.0, description='Game 2 description', age_limited=True)
+6.
+Game.objects.create(title='Game 3', cost=100.00, size=3.0, description='Game 3 description', age_limited=False)
+7.
+game1.buyer.set([buyer1, buyer2, buyer3])
+8.
+game2.buyer.set([buyer1, buyer2])
+9.
+game3.buyer.set([buyer1, buyer3])
+10.
+Buyer.objects.filter(age__lt=18)
+11.
+Game.objects.filter(age_limited=True)
+12.
+games_with_age_limit.exclude(buyer=under_18_buyer).update(buyer=buyer1)
+
+
+"""
