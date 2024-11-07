e604153a25a351a6d73cc21fe200fc8450e919ee
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-11-07 14:38:08 +0300

    Update models.py

--- DjangoMod_19/task1/models.py
+++ DjangoMod_19/task1/models.py
@@ -14,9 +14,7 @@ class Game(models.Model):
     size = models.DecimalField(decimal_places=2)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Buyer)
+    buyer = models.ManyToManyField(Buyer, related_name="games")
 
     def __str__(self):
         return self.title
-
-
